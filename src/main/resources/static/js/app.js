222b49d7615317c622ad5d3a52156ee0f71a2d24
shabutora <tsukasa@iyher.net>
2015-09-11 17:44:50 +0900

    Edit registry ui frontend screen.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,24 @@
-/**
- * Created by d450184 on 2015/09/11.
- */
+var app = angular.module("paraponera", ["ngResource"]);
+
+app.controller("host_ctrl", function($scope, $location, $http, $filter, $log){
+    $http({ method: 'GET', url: './hosts'})
+        .success(function (hosts) {
+            $scope.hosts = hosts;
+        }).error(function (msg) {
+            console.error(msg);
+        });
+});
+
+app.controller("image_ctrl", function($scope, $location, $http, $filter, $log, $resource){
+    $http({ method: 'GET', url: './images/' + $scope.host.name, "Content-Type": 'application/json'})
+        .success(function (images) {
+            console.log(images);
+            $scope.images = images;
+        }).error(function (msg) {
+            console.error(msg);
+        });
+});
+
+app.controller("tag_ctrl", function ($scope) {
+    $scope.tags = $scope.image.tag_list.sort();
+});
